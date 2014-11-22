99ff8fd1d757da1dde04c346a6858fd8ad786c19
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-11-22 23:50:35 +0900

    headerの充実

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * config
+ */
+add_theme_support('post-thumbnails');
+
+
+/*
+ * @return site name and article title if necessary
+ */
+function get_title_element_text() {
+    return wp_title(' | ', false, 'right') . get_bloginfo('name', false);
+}
+
+
+/*
+ * @return article thumbnail url or null
+ */
+function get_thumbnail_url() {
+    if (is_front_page()) {
+        return NULL;
+    }
+    $image_id = get_post_thumbnail_id();
+    $image_props = wp_get_attachment_image_src($image_id, true);
+    if (count($image_props) > 0) {
+        return $image_props[0];
+    } else {
+        return NULL;
+    }
+}
--- header.php
+++ header.php
@@ -1,7 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-<meta charset="<?php bloginfo('charset') ?>">
+<!-- meta -->
+<meta charset="<?php bloginfo('charset') ?>" />
+<meta name="description" content="<?php bloginfo('description') ?>" />
+<title><?php echo get_title_element_text() ?></title>
+
+<!-- ios meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="apple-mobile-web-app-capable" content="no" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+<!-- ogp -->
+<meta property="og:url" content="<?php echo is_front_page() ? home_url() : the_permalink() ?>" />
+<meta property="og:type" content="<?php echo is_front_page() ? 'website' : 'article' ?>" />
+<?php if (is_front_page()) : ?>
+<meta property="og:image" content="<?php echo bloginfo('template_url') ?>/images/ogp.jpg" />
+<?php endif ?>
+<?php if ($url = get_thumbnail_url()) : ?>
+<meta property="og:image" content="<?php echo $url ?>" />
+<?php endif ?>
+<meta property="og:title" content="<?php echo get_title_element_text() ?>" />
+<meta property="og:site_name" content="<?php bloginfo('name') ?>" />
+<meta property="og:description" content="<?php bloginfo('description') ?>" />
+<meta property="og:locale" content="ja_JP" />
+
+<!-- stylesheets -->
+<link rel="stylesheet" href="<?php echo bloginfo('template_url') ?>/style.css" />
+
+<!-- wp head -->
 <?php wp_head() ?>
 </head>
 <body>
